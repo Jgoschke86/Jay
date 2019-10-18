3d9ce25f7982703e6ffccaaa8a4a7ec8bbbf920f
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2019-10-18 16:33:50 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def cls():
 
 #Player inputs word to guess
 while True:
-    word = input("What word would you like? ")
+    word = input("What word would you like? ").lower()
     letter = []
     dash = []
     already_guessed = []
@@ -35,7 +35,7 @@ while True:
             
                     #Player guesses letter
         else:
-            guess_letter = input("What letter would you like to guess? ")
+            guess_letter = input("What letter would you like to guess? ").lower()
             cls()
             print("You have guessed " + str(guess_letter))
             
@@ -43,15 +43,15 @@ while True:
             if guess_letter not in letter:
                 if guess_letter in already_guessed:
                     print("You have already guessed that letter. Please choose a different letter.")
-                    print("You have already guessed " + str(already_guessed))
-                    guess_count += 1
+                    print("Letters already guessed " + str(already_guessed))
+
                 else:
                     already_guessed.append(guess_letter)    #Adds letter to list
                     already_guessed.sort()
                     print("Not in the word, please guess again")
                     guess_count += 1
         
-            #If in word
+                    #If in word
             elif guess_letter in letter:
                 print("Nice! That letter is in the word!")
                 while True:
@@ -65,18 +65,27 @@ while True:
         
                     else:
                         break
-                
+    cls()            
     print("Congratulations! You won!")
-    print("It took you " + str(guess_count) + " guesses!")
-    again = input("Would you like to play again? y/n ")
+    print("The word was " + word)
+    if guess_count == 1:
+        print("It took you " + str(guess_count) + " guess!")
+    else:
+        print("It took you " + str(guess_count) + " guesses!")
     
-    if (again == "y"):
-        print("Ok then")
-    elif (again == "yes"):
-        print("Ok then")
-    elif (again == "no"):
-        print("Thanks for playing!!")
-        break
-    elif (again == "n"):
-        print("Thanks for playing!!")
+    
+    pagain = ["y", "yes"]
+    nagain = ["n", "no"]
+    breaker = False
+    while True:
+        again = input("Would you like to play again? y/n ").lower()
+        if again in nagain:
+            print("Thanks for playing!!")
+            breaker = True
+            break
+        elif again in pagain:
+            break
+        else:
+            print("Please enter a valid response.")
+    if breaker:
         break
