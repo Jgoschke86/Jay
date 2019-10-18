d48acd15cc1eeb0cfeb01abf2356afe1631c13aa
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2019-10-18 12:56:28 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-play = True
+
+import os
+
+def cls():
+    os.system("cls")
+
+
+
 #Player inputs word to guess
-while play == True:
+while True:
     word = input("What word would you like? ")
     letter = []
     dash = []
-
-    
+    already_guessed = []
+    guess_count = 0
+    cls()
     #Seperates word into seperate letters in list letter
     for x in word:
         letter.append(x)
@@ -15,40 +23,60 @@ while play == True:
     for x in letter:
         dash.append("_")
     
-    #Guessing starts
+    #Guessing loop
     while True:
+        
         print("The word is:")
         print(" ".join(map(str, dash)))
         
+                    #Checks to see if any letters are still available and ends guess loop if none
         if letter.count("@") == len(letter):
             break
-        
+            
+                    #Player guesses letter
         else:
             guess_letter = input("What letter would you like to guess? ")
-        
-        
-        
+            cls()
+            print("You have guessed " + str(guess_letter))
+            
+                    #Checks to see if letter has already been guessed or if in word
             if guess_letter not in letter:
-                print("Not in the word, please guess again")
+                if guess_letter in already_guessed:
+                    print("You have already guessed that letter. Please choose a different letter.")
+                    print("You have already guessed " + str(already_guessed))
+                    guess_count += 1
+                else:
+                    already_guessed.append(guess_letter)    #Adds letter to list
+                    already_guessed.sort()
+                    print("Not in the word, please guess again")
+                    guess_count += 1
         
-        
+            #If in word
             elif guess_letter in letter:
-                print("Nice! That letter is in the word. Keep going!")
+                print("Nice! That letter is in the word!")
                 while True:
                     if guess_letter in letter:
                         correct = letter.index(guess_letter)
-                        dash[correct] = guess_letter
-                        letter[correct] = "@"
+                        dash[correct] = guess_letter        #Converts dash to letter
+                        letter[correct] = "@"               #Converts letter in "letter list" to @
+                        guess_count += 1
+                        already_guessed.append(guess_letter)#Adds letter to list
+                        already_guessed.sort()
+        
                     else:
                         break
                 
     print("Congratulations! You won!")
-    again = input("Would you like to play again?")
-    print(again)
-    if again == "y" or "yes":
+    print("It took you " + str(guess_count) + " guesses!")
+    again = input("Would you like to play again? y/n ")
+    
+    if (again == "y"):
         print("Ok then")
-        play == True
-    else:
-        again == "n" or "no"
+    elif (again == "yes"):
+        print("Ok then")
+    elif (again == "no"):
         print("Thanks for playing!!")
-        play == False
+        break
+    elif (again == "n"):
+        print("Thanks for playing!!")
+        break
