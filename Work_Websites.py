879055500da8df82cefb7c4bc7406d748aef3a24
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2019-11-19 22:19:18 -0500

    Update Work_Websites.py

--- Work_Websites.py
+++ Work_Websites.py
@@ -1,18 +1,41 @@
 from tkinter import *
+import xlrd
+
+
+def about_window():
+    top = Toplevel()
+    top.title("About")
+    about_text = Label(top, text = """This program was made to assist with accessing certain websites.
+    Created by Justin Goschke
+    v1.0""", font = 15)
+    about_text.pack()
+    back_button = Button(top, text = "Back", command = top.destroy, width = 15, font = 16, bg = "dark gray")
+    back_button.pack()
+    top.mainloop()
+
+
 
 root = Tk()
 
 root.title("Website Shortcuts")
 
 
+
+
+menu_bar = Menu(root)
+file = Menu(menu_bar, tearoff = 0)
+menu_bar.add_cascade(label = "File", menu = file)
+file.add_command(label = "About", command = about_window)
+file.add_command(label = "Exit", command = root.destroy)
+
 username = []
 password = []
 website = []
 
-top_frame = Frame(root, bg = "gray")
+top_frame = Frame(root)
 top_frame.pack()
 
-top_label = Label(top_frame, text = "Warranty Site Logins", font = 26, bg = "gray")
+top_label = Label(top_frame, text = "Warranty Site Logins", font = 26)
 top_label.grid(columnspan = 4)
 
 
@@ -24,6 +47,7 @@ kohler = Button(top_frame, text = "Kohler", width = 15, font = 16, bg = "dark gr
 scag = Button(top_frame, text = "Scag", width = 15, font = 16, bg = "dark gray")
 schiller = Button(top_frame, text = "Schiller", width = 15, font = 16, bg = "dark gray")
 toro = Button(top_frame, text = "Toro", width = 15, font = 16, bg = "dark gray")
+
 
 briggs.grid(row = 1, column = 0)
 exmark.grid(row = 1, column = 1)
@@ -41,5 +65,5 @@ bottom_box.insert(3, "Password = " + str(password))
 bottom_box.grid(columnspan = 5)
 
 
-
+root.config(menu = menu_bar)
 root.mainloop()
