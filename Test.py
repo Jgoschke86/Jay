7951cbeac2d231032ab9e2d8ae3ca04e2d8c7b1f
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2019-11-21 21:59:25 -0500

    c

---
+++ Test.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\jgosc\Documents\GitHub\Jay\chromedriver.exe")
+
+driver.get("https://www.thepowerportal.com/Login.htm")
