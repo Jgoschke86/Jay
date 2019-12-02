a90bdadd6c79056a51d2ecb312988a2b84c32531
Jgoschke86 <Jgoschke86@gmail.com>
2019-12-02 11:12:36 -0500

    Update Work_shortcuts.py

--- Work_shortcuts.py
+++ Work_shortcuts.py
@@ -2,9 +2,10 @@ from tkinter import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import xlrd
+import time
 
 #Opens the excel sheet to read it
-intwarr = xlrd.open_workbook(r"C:\Users\jgoschke\Downloads\INTERNET WARRANTY SIGN ONS.xls")
+intwarr = xlrd.open_workbook(r"S:\OPE\OPE Service\INTERNET WARRANTY SIGN ONS.xls")
 sheet = intwarr.sheet_by_index(0)
 cell = sheet.cell(0,3)
 
@@ -30,8 +31,8 @@ scg_usr = sheet.cell_value(195,1)
 scg_pass = sheet.cell_value(196,1)
 sch_usr = sheet.cell_value(191,1)
 sch_pass = sheet.cell_value(192,1)
-stl_usr = sheet.cell_value(214,1)
-stl_pass = sheet.cell_value(215,1)
+stl_usr = sheet.cell_value(22,1)
+stl_pass = sheet.cell_value(23,1)
 tor_usr = sheet.cell_value(230,1)
 tor_pass = sheet.cell_value(231,1)
 
@@ -40,12 +41,12 @@ tor_pass = sheet.cell_value(231,1)
 var = StringVar()
 Briggs = ["Briggs and Stratton", "https://www.thepowerportal.com/Login.htm", str(int(brg_usr)), str(brg_pass)]
 Exmark = ["Exmark", "https://user.exmark.com/login?returnUrl=https://extranet.exmark.com", str(int(exm_usr)), str(exm_pass)]
-Honda = ["Honda", "https://www.in.honda.com/", str(hon_usr), str(hon_pass)]
+Honda = ["Honda", "https://www.in.honda.com/", str(hon_usr), str(hon_pass), "310577"]
 Kawasaki = ["Kawasaki Engines", "https://www.kawasakipower.com/", str(kaw_usr), str(kaw_pass)]
 Kohler = ["Kohler Engines", "https://pswusers.arinet.com/Kohler", str(int(koh_usr)), str(koh_pass)]
-Scag = ["Scag Mowers", "www.scagtech.com", str(scg_usr), str(scg_pass)]
+Scag = ["Scag Mowers", "http://www.scagtech.com", str(scg_usr), str(scg_pass)]
 Schiller = ["Schiller/Ryan/Bobcat", "https://www.schillergcpro.com", str(sch_usr), str(sch_pass)]
-Stihl = ["Stihl", "https://www.stihleservice.com", str(stl_usr), str(stl_pass)]
+Stihl = ["Stihl", "https://dealers.stihlusa.com/Account", str(stl_usr), str(stl_pass)]
 Toro = ["Toro", "http://dealer.thetorocompany.com/", str(tor_usr), str(tor_pass)]
 
 
@@ -71,12 +72,15 @@ def execute(evt):
         pass_input = driver.find_element_by_xpath("//*[@id=\"Password\"]")
         pass_input.send_keys(Exmark[3])
         login_button = driver.find_element_by_xpath("//*[@id=\"form0\"]/div/div/div[3]/div/p/input").click()
-    # elif details == "Honda":
-    #     driver.get(Honda[1])
-    #     name_input = pass
-    #     name_input.send_keys(Honda[2])
-    #     pass_input = pass
-    #     pass_input.send_keys(Honda[3])
+    elif details == "Honda":
+        driver.get(Honda[1])
+        dlr_input = driver.find_element_by_xpath("/html/body/form/table[2]/tbody/tr[2]/td[2]/input")
+        dlr_input.send_keys(Honda[4])
+        name_input = driver.find_element_by_xpath("//*[@id=\"txtLogonID\"]")
+        name_input.send_keys(Honda[2])
+        pass_input = driver.find_element_by_xpath("//*[@id=\"txtPassword\"]")
+        pass_input.send_keys(Honda[3])
+        login_button = driver.find_element_by_xpath("//*[@id=\"btnLogon\"]").click()
     elif details == "Kawasaki":
         driver.get(Kawasaki[1])
         name_input = driver.find_element_by_xpath("//*[@id=\"W_USERNAME\"]")
@@ -84,6 +88,8 @@ def execute(evt):
         pass_input = driver.find_element_by_xpath("//*[@id=\"LW3USRPAS\"]")
         pass_input.send_keys(Kawasaki[3])
         login_button = driver.find_element_by_xpath("//*[@id=\"Login\"]").click()
+    elif details == "Kohler":
+        driver.get(Kohler[1])
     elif details == "Scag":
         driver.get(Scag[1])
         name_input = driver.find_element_by_xpath("//*[@id=\"txtUserName\"]")
@@ -91,8 +97,27 @@ def execute(evt):
         pass_input = driver.find_element_by_xpath("//*[@id=\"txtUserPass\"]")
         pass_input.send_keys(Scag[3])
         login_button = driver.find_element_by_xpath("//*[@id=\"Button1\"]").click()
-
-
+    elif details == "Schiller":
+        driver.get(Schiller[1])
+        name_input = driver.find_element_by_xpath("//*[@id=\"sgcuser\"]")
+        name_input.send_keys(Schiller[2])
+        pass_input = driver.find_element_by_xpath("//*[@id=\"sgcpass\"]")
+        pass_input.send_keys(Schiller[3])
+        login_button = driver.find_element_by_xpath("//*[@id=\"sgcDoLogin\"]").click()
+    elif details == "Stihl":
+        driver.get(Stihl[1])
+        name_input = driver.find_element_by_xpath("//*[@id=\"username\"]")
+        name_input.send_keys(Stihl[2])
+        pass_input = driver.find_element_by_xpath("//*[@id=\"password\"]")
+        pass_input.send_keys(Stihl[3])
+        login_button = driver.find_element_by_xpath("//*[@id=\"content\"]/div/div/div/form/p[4]/input").click()
+    elif details == "Toro":
+        driver.get(Toro[1])
+        name_input = driver.find_element_by_xpath("//*[@id=\"username\"]")
+        name_input.send_keys(Toro[2])
+        pass_input = driver.find_element_by_xpath("//*[@id=\"password\"]")
+        pass_input.send_keys(Toro[3])
+        login_button = driver.find_element_by_xpath("/html/body/div/div[2]/div/form/div[6]/a").click()
 # changes display in info_box based on what is selected for manufacturer
 def on_selection(evt):
     selected = man_box.curselection()
