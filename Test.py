fdb751cdfc03519be43f867a9962e3e8acea29b0
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2020-01-03 20:58:17 -0500

    c

--- Test.py
+++ Test.py
@@ -1,2 +1,7 @@
-import os
-print(os.path)
+from random import randint
+
+word_list = open(file = r"C:\Users\jgosc\Documents\GitHub\Jay\Games\Hangman\word_list.txt")
+secret_word = word_list.readlines(20)
+
+print(secret_word)
+word_list.close()
