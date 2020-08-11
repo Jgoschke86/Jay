647a9d84b74d6e1703547dedc72b3239d29e1859
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2020-08-11 12:06:11 -0400

    c

--- Work/AssemblyLog.py
+++ Work/AssemblyLog.py
@@ -4,19 +4,22 @@ from openpyxl.xml.constants import MAX_ROW
 
 print("Opening Excel and reading file......")
 wb = openpyxl.load_workbook(r"C:\Python Stuff\Test Log.xlsx")
+time_sheet = wb.worksheets[9]
 
 names = wb.sheetnames
 
 names.remove("Time")
-names.remove("")
-names.remove("")
-names.remove("")
+names.remove("Set up Checklist")
+names.remove("Extra")
 
-print("Current names to scan.... " + names)
-sheet = wb.active
+print("Current names to scan.... ")
+print(names)
+ws = wb.worksheets[1]
+start_cell = 10
+row_numbers = ws.max_row
+print("There are " + str(row_numbers) + " cells to scan")
 
-
-
-for i in range(1,590):
-    cell_obj = sheet.cell(row = i, column = 1)
-    print(cell_obj.value)
+print("Scanning cells.......")
+for i in range(start_cell,row_numbers):
+    cell_obj = ws.cell(row = i, column = 3)
+    if cell_obj in time_sheet["A"]:
