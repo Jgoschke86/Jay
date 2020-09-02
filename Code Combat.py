2f25fb13743aafd425aeee6865fad27055fbdc42
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2020-09-02 16:16:28 -0400

    c

--- Code Combat.py
+++ Code Combat.py
@@ -4,3 +4,25 @@ itemPosition = item.pos
 itemX = itemPosition.x
 itemY = itemPosition.y
 hero.moveXY(itemX, ItemY)
+or
+item = hero.findNearestItem()
+hero.moveXY(item.pos.x, item.pos.y)
+
+#say something
+hero.say("say something")
+
+
+#attack an enemy
+hero.attack("enemy name" or function)
+
+
+#build someting at x,y
+hero.buildXY("type", x,y)
+
+
+#flag moving
+flag = hero.findFlag()
+if flag:
+    hero.pickUpFlag(flag)
+else:
+    hero.say("Place a flag for me to go to.")
