54709af61ea9baf1719b3ed12a40f6edd2098916
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2020-11-03 12:49:08 -0500

    Create CtoF.py

---
+++ CtoF.py
@@ -0,0 +1,27 @@
+# close = ["close", "exit", "quit", "q"]
+
+# while True:
+#     cel = input("What is the temp in Celsius? ")
+#     if cel.lower() in close:
+#         break
+#     elif cel.isdigit():
+#         f = 9 * int(cel) / 5 + 32
+#         print("The temperature in Fahrenheit is ", f)
+#     else:
+#         print("Please enter a valid number.")
+
+import sys
+
+while True:
+    if len(sys.argv) == 1:
+        c = input("what is the degree ")
+    else:
+        c = sys.argv[1]
+    if c.lower() in ("q", "quit"):
+        print("Goodbye")
+        break
+    else:
+        f = 9 * int(c) / 5 + 32
+        print(" the temp is", f)
+        if len(sys.argv) != 1:
+            break
