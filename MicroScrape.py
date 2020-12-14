820aa961a91869d6e789b80ee735b1034eaac5fa
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2020-12-14 18:53:48 -0500

    c

--- MicroScrape.py
+++ MicroScrape.py
@@ -3,9 +3,11 @@ import pandas as pd
 import requests
 
 
-page = requests.get("https://www.microcenter.com/search/search_results.aspx?N=4294966995&NTK=all&sortby=pricelow&rpp=96")
-source = page.content
-soup = BeautifulSoup(source, "lxml")
+# page = requests.get("https://www.microcenter.com/search/search_results.aspx?N=4294966995&NTK=all&sortby=pricelow&rpp=96")
+# source = page.content
+# soup = BeautifulSoup(source, "lxml")
+soup = BeautifulSoup(open(r"C:/Users/jgosc/Downloads/Processors_CPUs _ Micro Center.html"), "html.parser")
+
 
 amd_proc = {}
 intel_proc = {}
@@ -19,22 +21,22 @@ for link in links:
             name = link.get("data-name")
             name = ' '.join(name.split()[:3])
             price = link.get("data-price")
-            # print(name, price)
             amd_proc[name] = price
-# for link in links:
-#     if "Intel" in link.text:
-#         if link.get("data-name") == None:
-#             pass
-#         else:
-#             name = link.get("data-name")
-#             name = ' '.join(name.split()[:2])
-#             price = link.get("data-price")
-#             intel_proc[name] = price
+            # print(name, price)
+for link in links:
+    if "Intel" in link.text:
+        if link.get("data-name") == None:
+            pass
+        else:
+            name = link.get("data-name")
+            name = ' '.join(name.split()[:2])
+            price = link.get("data-price")
+            intel_proc[name] = price
             # print(name, price)
 # print(amd_proc)
 # print(intel_proc)
 
 amd_db = pd.DataFrame({"Name" : list(amd_proc.keys()), "Price" : list(amd_proc.values())})
 print(amd_db)
-intel_db = pd.DataFrame({"Name" : intel_proc.keys(), "Price" : intel_proc.values()})
+intel_db = pd.DataFrame({"Name" : list(intel_proc.keys()), "Price" : list(intel_proc.values())})
 print(intel_db)
