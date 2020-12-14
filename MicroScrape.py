e8e1dfec23d68f9f00c7f5d2a47b10408e873611
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2020-12-14 18:28:16 -0500

    c

--- MicroScrape.py
+++ MicroScrape.py
@@ -21,20 +21,20 @@ for link in links:
             price = link.get("data-price")
             # print(name, price)
             amd_proc[name] = price
-for link in links:
-    if "Intel" in link.text:
-        if link.get("data-name") == None:
-            pass
-        else:
-            name = link.get("data-name")
-            name = ' '.join(name.split()[:2])
-            price = link.get("data-price")
-            intel_proc[name] = price
+# for link in links:
+#     if "Intel" in link.text:
+#         if link.get("data-name") == None:
+#             pass
+#         else:
+#             name = link.get("data-name")
+#             name = ' '.join(name.split()[:2])
+#             price = link.get("data-price")
+#             intel_proc[name] = price
             # print(name, price)
 # print(amd_proc)
 # print(intel_proc)
 
-amd_db = pd.DataFrame({"Name" : amd_proc.keys(), "Price" : amd_proc.values()})
+amd_db = pd.DataFrame({"Name" : list(amd_proc.keys()), "Price" : list(amd_proc.values())})
 print(amd_db)
 intel_db = pd.DataFrame({"Name" : intel_proc.keys(), "Price" : intel_proc.values()})
 print(intel_db)
