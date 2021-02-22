deba139c2aa27c5cc3c7e9779f7bbadc7d4ace25
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2021-02-22 11:55:14 -0500

    Update MicroScrape.py

--- WebScrapers/Processors/MicroScrape.py
+++ WebScrapers/Processors/MicroScrape.py
@@ -24,12 +24,16 @@ vid_content = pd.DataFrame(vid_file)
 
 amd_proc_list = amd_content['Name'].tolist()
 intel_proc_list = intel_content['Name'].tolist()
+vid_list = vid_content['Name'].tolist()
 append_to_csv = {}
 amd_proc = {}
 intel_proc = {}
+vid_card = {}
 
-links = proc_soup.find_all("a")
-for link in links:
+proc_links = proc_soup.find_all("a")
+vid_links = vid_soup.find_all("a")
+
+for link in proc_links:
     if "AMD" in link.text:
         if link.get("data-name") == None:
             pass
@@ -46,6 +50,16 @@ for link in links:
             name = ' '.join(name.split()[:2])
             price = link.get("data-price")
             intel_proc[name] = price
+    links = proc_soup.find_all("a")
+for link in vid_links:
+    if link.get("data-name") == None:
+        pass
+    else:
+        name = link.get("data-name")
+        brand = link.get("data-brand")
+        name = brand, " ", name
+        price = link.get("data-price")
+        vid_card[name] = price
 
 for key,value in amd_proc.items():
     new_item = {key:value}
@@ -63,13 +77,22 @@ for key,value in intel_proc.items():
         print(new_item, " added")
         append_to_csv["Name"] = key
         intel_content = intel_content.append(append_to_csv, ignore_index=True)
+for key,value in vid_card.items():
+    new_item = {key:value}
+    if key in vid_list:
+        pass
+    else:
+        print(new_item, " added")
+        append_to_csv["Name"] = key
+        vid_content = vid_content.append(append_to_csv, ignore_index=True)
 
 amd_content[date.strftime("%x")] = amd_content["Name"].map(amd_proc)
 intel_content[date.strftime("%x")] = intel_content["Name"].map(intel_proc)
-
+vid_content[date.strftime("%x")] = vid_content["Name"].map(vid_content)
 
 amd_content = amd_content.sort_values(by = ["Name"])
 intel_content = intel_content.sort_values(by = ["Name"])
+vid_content = vid_content.sort_values(by = ["Name"])
 
 print(amd_content)
 print(intel_content)
