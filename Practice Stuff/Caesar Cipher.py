5273bd531c09c3a9bf4b54c03a09d450f814072f
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2021-06-03 12:33:36 -0400

    Update Caesar Cipher.py

--- Practice Stuff/Caesar Cipher.py
+++ Practice Stuff/Caesar Cipher.py
@@ -1,50 +1,123 @@
-print(29%26)
-user_word = input("what do you want encrypted?  ")
-user_cipher = input("what cipher do you want?   ")
-abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-new_word = []
-is_cap = []
-loc = []
-new_loc = []
-s = ""
+do_undo = input("""1.Convert to cipher
+2.Undo Cipher
+""")
+
 
 def split_word(word):
     return [char for char in word]
 
-split_string = split_word(user_word)
-shift = int(user_cipher)
+#  Converts input into Cipher
+if do_undo == "1":
+    user_word = input("what do you want encrypted?  ")
+    user_cipher = input("what cipher do you want?   ")
 
-#  Converts letters to numerical location in alphabet
-for letter in split_string:
-    if letter.lower() in abc:
-        loc.append(abc.index(letter.lower()))
-    else:
-        loc.append(letter)
-    
+    #  Alphabet, altered letters based on ciper number, location of capital letters, location of letters in alpha, altered location based on cipher, join method
+    abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    new_word = []
+    is_cap = []
+    loc = []
+    new_loc = []
+    s = ""
 
-#  Converts to new alphabet location
-for i in loc:
-    is_int = isinstance(i, int)
-    if is_int == True:
-        new_num = i + shift
-        if new_num > 26:
-            new_num = new_num % 26
-    else:
-        new_num = i
-    new_loc.append(new_num)
+    split_string = split_word(user_word)
+    shift = int(user_cipher)
 
-#  Converts new location to letters
-for num in new_loc:
-    is_int = isinstance(num, int)
-    if is_int == True:
-        new_letter = abc[num]
-        new_word.append(new_letter)
-    else:
-        new_word.append(num)
+    #  Converts letters to numerical location in alphabet and checks for capitalization
+    for letter in split_string:
+        if letter.lower() in abc:
+            loc.append(abc.index(letter.lower()))
+        else:
+            loc.append(letter)
+        if letter.isupper() == True:
+            is_cap.append(split_string.index(letter))
 
-#  Joins letters to make new sentence
-new_sent = s.join(new_word)
-print(new_sent)
+    #  Converts to new alphabet location
+    for i in loc:
+        is_int = isinstance(i, int)
+        if is_int == True:
+            new_num = i + shift
+            if new_num > 26:
+                new_num = new_num % 26
+        else:
+            new_num = i
+        new_loc.append(new_num)
+
+    #  Converts new location to letters
+    for num in new_loc:
+        is_int = isinstance(num, int)
+        if is_int == True:
+            new_letter = abc[num]
+            new_word.append(new_letter)
+        else:
+            new_word.append(num)
+
+    #  Converts letters back into capitals
+    for cap_loc in is_cap:
+        word = new_word[cap_loc]
+        new_word[cap_loc] = word.upper()
+
+    #  Joins letters to make new sentence
+    new_sent = s.join(new_word)
+    print(new_sent)
 
 
 
+#  Converts cipher back to normal
+if do_undo == "2":
+    og_word = input("""What do you want undone?
+""")
+
+    abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    is_cap = []
+    count = 1
+    loc = []
+    split_string = split_word(og_word)
+
+    #  Converts letters to numerical location in alphabet and checks for capitalization
+    for letter in split_string:
+        if letter.lower() in abc:
+            loc.append(abc.index(letter.lower()))
+        else:
+            loc.append(letter)
+        if letter.isupper() == True:
+            is_cap.append(split_string.index(letter))
+
+    for i in range(27):
+        new_word = []
+        new_loc = []
+        s = ""
+
+        #  Converts to new alphabet location
+        for i in loc:
+            is_int = isinstance(i, int)
+            if is_int == True:
+                new_num = i + count
+                print(new_num)
+                if new_num >= 26:
+                    new_num = new_num - 25
+                else:
+                    new_num + 8
+                print(new_num)
+            else:
+                new_num = i
+            new_loc.append(new_num)
+
+        #  Converts new location to letters
+        for num in new_loc:
+            is_int = isinstance(num, int)
+            if is_int == True:
+                new_letter = abc[num]
+                new_word.append(new_letter)
+            else:
+                new_word.append(num)
+
+        #  Converts letters back into capitals
+        for cap_loc in is_cap:
+            word = new_word[cap_loc]
+            new_word[cap_loc] = word.upper()
+
+        #  Joins letters to make new sentence
+        new_sent = s.join(new_word)
+        print(count)
+        print(new_sent)
+        count += 1
