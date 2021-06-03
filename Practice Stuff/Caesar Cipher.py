96f3f01e44870f62b0b132b4ef90cbe754db85e1
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2021-06-02 22:01:45 -0400

    Update Caesar Cipher.py

--- Practice Stuff/Caesar Cipher.py
+++ Practice Stuff/Caesar Cipher.py
@@ -3,23 +3,26 @@ user_word = input("what do you want encrypted?  ")
 user_cipher = input("what cipher do you want?   ")
 abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 new_word = []
+is_cap = []
 loc = []
 new_loc = []
-count = 0
 s = ""
 
 def split_word(word):
     return [char for char in word]
 
-# def caesar(sent, place):
 split_string = split_word(user_word)
 shift = int(user_cipher)
+
+#  Converts letters to numerical location in alphabet
 for letter in split_string:
-    if letter in abc:
-        loc.append(abc.index(letter))
+    if letter.lower() in abc:
+        loc.append(abc.index(letter.lower()))
     else:
         loc.append(letter)
-print(loc)
+    
+
+#  Converts to new alphabet location
 for i in loc:
     is_int = isinstance(i, int)
     if is_int == True:
@@ -29,18 +32,19 @@ for i in loc:
     else:
         new_num = i
     new_loc.append(new_num)
-print(new_loc)
-for letter in split_string:
-    if letter in abc:
-        new_letter = abc[new_loc[count]]
+
+#  Converts new location to letters
+for num in new_loc:
+    is_int = isinstance(num, int)
+    if is_int == True:
+        new_letter = abc[num]
         new_word.append(new_letter)
     else:
-        new_word.append(letter)
-    count += 1
-print(new_word)
+        new_word.append(num)
 
+#  Joins letters to make new sentence
 new_sent = s.join(new_word)
 print(new_sent)
-# cipher_message = caesar(user_word, user_cipher)
+
 
 
