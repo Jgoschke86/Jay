da61dc6e51289439c0ed6caf00a31ef59de1deca
Jgoschke86 <56652104+Jgoschke86@users.noreply.github.com>
2022-02-27 20:19:17 -0500

    c

--- Work/Assembly Log/AssemblyLog.py
+++ Work/Assembly Log/AssemblyLog.py
@@ -16,23 +16,3 @@ names = wb.sheetnames
 names.remove("Time")
 names.remove("Set up Checklist")
 names.remove("Extra")
-
-
-# print("Current names to scan.... ")
-# print(names)
-
-print("Scanning cells.......")
-for sheet in names:
-    print(sheet) # Prints all sheet names to cycle through
-    for row in ws.iter_rows(min_row=12, max_row=30, min_col=3, max_col=3, values_only=True): # Goes through each row in worksheet
-        for model_number in row: # Gets model from cell
-            model = str(model_number).lower()  # Makes model into string and lowercase to compare
-            for row in time_sheet.iter_rows(max_col=1): # Goes to time sheet and scans rows
-                for location in row: # Gets model from rom
-                    model_time = str(location).lower() # Converts model to string and lowercase
-                    print(location.absolute_coordinate)
-                    if model == model_time: # Compares both models together
-                        
-                        print("yes")
-
-                
